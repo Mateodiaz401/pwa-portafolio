35e7d3e92736a290581af654a36942f2528585d2
mateo <mateodiaz401@gmail.com>
2020-03-10 00:53:18 -0500

    modificaciones para ios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-util.js');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v3';
 const DYNAMYC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
